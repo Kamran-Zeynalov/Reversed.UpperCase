00834c615268ce716578a0858c1175b71d19441e
Kamran Z <kamraneze@code.edu.az>
2023-08-28 11:43:20 +0400

    String reversed and UpperCase

---
+++ String.Reverse.UppreCase/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace String.Reverse.UppreCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string random = "ey babek kebab ye";
+
+            char[] chars = random.ToCharArray();
+            Array.Reverse(chars);
+
+            string reversed = new(chars);
+            Console.WriteLine(reversed.ToUpper());
+
+        }
+    }
+}
